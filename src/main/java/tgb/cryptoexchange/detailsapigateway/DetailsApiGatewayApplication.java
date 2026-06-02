4b2e399321519a0708fba7ef1a046494f68dee98
GrewGh <grew.gh@mail.ru>
2026-06-02 19:56:53 +0300

    init

---
+++ src/main/java/tgb/cryptoexchange/detailsapigateway/DetailsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.detailsapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetailsApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgb/cryptoexchange/detailsapigateway/DetailsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.detailsapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailsApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
